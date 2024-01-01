04706e122720da60cefde7d89caa29e2232b8c31
Thinh Nguyen <149649225+thinh0rigami@users.noreply.github.com>
2024-01-01 17:28:41 +0700

    Create zsh.sh

---
+++ ez/prep/zsh.sh
@@ -0,0 +1,7 @@
+source $th0ri__prep/rec/sh/th0ri-prep.sh
+
+ln -fs $th0ri__prep/rec/sh/th0ri-prep.sh $HOME/.th0ri-prep.sh
+
+source $HOME/.th0ri-prep.sh
+
+echo "source $HOME/.th0ri-prep.sh" >> $HOME/.zshrc
