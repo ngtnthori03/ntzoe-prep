2ee2580d7a18878b4d0b097b67e935694f5c35b2
Thinh Nguyen <149649225+thinhxorigami@users.noreply.github.com>
2024-01-06 04:29:36 +0700

    Update zsh.sh

--- ez/prep/zsh.sh
+++ ez/prep/zsh.sh
@@ -1,5 +1,5 @@
-source $thxori_prep_dir/rec/sh/th0ri-prep.sh
+source $thxori_prep_dir/rec/sh/thxori-prep.sh
 
-ln -fs $thxori_prep_dir/rec/sh/th0ri-prep.sh $HOME/.thxori-prep.sh
+ln -fs $thxori_prep_dir/rec/sh/thxori-prep.sh $HOME/.thxori-prep.sh
 
 echo "source $HOME/.thxori-prep.sh" >> $HOME/.zshrc
